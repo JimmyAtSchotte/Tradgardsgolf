0df6e503b5611b0d45ae9b095045209cd25148fb
Jimmy <jimmy.mattsson@schotte.se>
2019-10-06 20:44:17 +0200

    Email validation

---
+++ Tradgardsgolf.Core/Validators/EmailValidator.cs
@@ -0,0 +1,16 @@
+﻿using EmailAddressLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tradgardsgolf.Core.Interfaces.Validators;
+
+namespace Tradgardsgolf.Core.Validators
+{
+    public class EmailValidator : IEmailValidator
+    {
+        public bool IsValidEmail(string email)
+        {
+            return EmailAddressValidator.JStedfast(email);
+        }
+    }
+}
