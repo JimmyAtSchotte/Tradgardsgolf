af66dd2345f03ab6e35e5d34b13b421ea5c1984e
Jimmy <jimmy.mattsson@schotte.se>
2020-05-03 21:36:58 +0200

    Course header controll added

---
+++ Tradgardsgolf.Mobile/Tradgardsgolf.Mobile/Play/CourseHeader.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tradgardsgolf.ApiClient.Course;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tradgardsgolf.Mobile.Play
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseHeader : ContentView
+    {
+        public static readonly BindableProperty CourseProperty = BindableProperty.Create("Course", typeof(Course), typeof(CourseHeader), null);
+
+        public Course Course
+        {
+            get => (Course)GetValue(CourseProperty);
+            set => SetValue(CourseProperty, value);
+        }
+
+        public CourseHeader()
+        {
+            InitializeComponent();
+
+            CourseName.SetBinding(Label.TextProperty, new Binding(nameof(Course.Name), source: Course));
+        }
+    }
+}
---
+++ Tradgardsgolf.Mobile/Tradgardsgolf.Mobile/Play/CourseQuickStats.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tradgardsgolf.Mobile.Play
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CourseQuickStats : ContentView
+    {
+        public CourseQuickStats()
+        {
+            InitializeComponent();
+        }
+    }
+}
