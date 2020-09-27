30df887ce63f4a4f597877f71a947b13002c494c
Jimmy <jimmy.mattsson@schotte.se>
2020-09-27 16:28:05 +0200

    HoleScoreCollection as a wrapper for List<HoleScore>

--- Tradgardsgolf.Blazor/Data/ScorecardState.cs
+++ Tradgardsgolf.Blazor/Data/ScorecardState.cs
@@ -7,6 +7,37 @@ using System.Threading.Tasks;
 
 namespace Tradgardsgolf.Blazor.Data
 {
+    public class HoleScoreCollection : IList<HoleScore>
+    {
+        private readonly IList<HoleScore> _holeScores;
+
+        public HoleScoreCollection()
+        {
+            _holeScores = new List<HoleScore>();
+        }
+
+        public HoleScore this[int index] { 
+            get =>  _holeScores.FirstOrDefault(x => x.Hole == index);            
+            set =>  this[index] = value;            
+        }
+
+        public int Count => _holeScores.Count;
+        public bool IsReadOnly => _holeScores.IsReadOnly;
+        public void Add(HoleScore item) => _holeScores.Add(item);
+        public void Clear() => _holeScores.Clear();
+        public bool Contains(HoleScore item) => _holeScores.Contains(item);
+        public void CopyTo(HoleScore[] array, int arrayIndex) => _holeScores.CopyTo(array, arrayIndex);
+        public IEnumerator<HoleScore> GetEnumerator() => _holeScores.GetEnumerator();
+
+        public int IndexOf(HoleScore item) => _holeScores.IndexOf(item);
+        public void Insert(int index, HoleScore item) => _holeScores.Insert(index, item);
+
+        public bool Remove(HoleScore item) => _holeScores.Remove(item);
+        public void RemoveAt(int index) => _holeScores.RemoveAt(index);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+
     public class HoleScore
     {
         public int Hole { get; set; }
@@ -33,7 +64,7 @@ namespace Tradgardsgolf.Blazor.Data
     {
         public string Name { get; set; }
 
-        public IList<HoleScore> Scores { get; set; }
+        public HoleScoreCollection Scores { get; set; }
 
 
         public PlayerScore()
@@ -41,7 +72,7 @@ namespace Tradgardsgolf.Blazor.Data
 
         }
 
-        private PlayerScore(string name, IList<HoleScore> scores)
+        private PlayerScore(string name, HoleScoreCollection scores)
         {
             Name = name;
             Scores = scores;
@@ -49,7 +80,7 @@ namespace Tradgardsgolf.Blazor.Data
 
         public static PlayerScore Create(string name, int holes)
         {
-            var scores = new List<HoleScore>();
+            var scores = new HoleScoreCollection();
 
             for (int hole = 1; hole <= holes; hole++)
                 scores.Add(HoleScore.Create(hole));
