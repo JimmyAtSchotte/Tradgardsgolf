d0ea4d6f7ed31b448e9cdd4d6c8748bfa1538642
Jimmy <jimmy.mattsson@schotte.se>
2021-01-07 21:35:40 +0100

    Moved location to a cascading component

--- Tradgardsgolf.Api.Shared/CourseModel.cs
+++ Tradgardsgolf.Api.Shared/CourseModel.cs
@@ -9,9 +9,7 @@ namespace Tradgardsgolf.Api.Shared
         public int Holes { get; set; }
         public double Longitude { get; set;  }
         public double Latitude { get; set; }
-        
         public PlayerModel CreatedBy { get; set; }
-        
         public DateTime Created { get; set; }
         public string Image { get; set; }
         public int SeasonTableRounds => 6;
