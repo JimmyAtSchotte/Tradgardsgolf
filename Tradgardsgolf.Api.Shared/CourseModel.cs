cfb5cd6b6507008cfc15e97c8e9e32f3e85b3b3f
Jimmy <jimmy.mattsson@schotte.se>
2021-01-08 20:10:48 +0100

    Reordering course list on location change

--- Tradgardsgolf.Api.Shared/CourseModel.cs
+++ Tradgardsgolf.Api.Shared/CourseModel.cs
@@ -13,5 +13,19 @@ namespace Tradgardsgolf.Api.Shared
         public DateTime Created { get; set; }
         public string Image { get; set; }
         public int SeasonTableRounds => 6;
+        
+        public double GetDistance(double longitude, double latitude)
+        {
+            if (Name == "Testbanan")
+                return 0;
+
+            var d1 = this.Latitude * (Math.PI / 180.0);
+            var num1 = this.Longitude * (Math.PI / 180.0);
+            var d2 = latitude * (Math.PI / 180.0);
+            var num2 = longitude * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+
+            return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+        }
     }
 }
