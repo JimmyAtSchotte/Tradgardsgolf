7f2fefbfe2c9032b5fd2f991a0e068754153d723
Jimmy <vaugh@hotmail.se>
2023-12-21 07:14:02 +0100

    Refactor SeedData into static extension

--- Tradgardsgolf.Api/Program.cs
+++ Tradgardsgolf.Api/Program.cs
@@ -31,7 +31,7 @@ namespace Tradgardsgolf.Api
                 await using var context = scope.ServiceProvider.GetService<TradgardsgolfContext>();
 
                 if (context.Database.IsInMemory())
-                    await SeedData(context);
+                    await context.SeedData();
                 
                 
                 await host.RunAsync();
@@ -46,47 +46,7 @@ namespace Tradgardsgolf.Api
             }      
         }
 
-        private static async Task SeedData(TradgardsgolfContext context)
-        {
-            var jimmy = Player.Create(p => p.Name = "Jimmy");
-            var patrik = Player.Create(p => p.Name = "Patrik");
-            
-            context.Add(jimmy);
-            context.Add(patrik);
-            
-            var kumhof = Course.Create(jimmy, p =>
-            {
-                p.Holes = 6;
-                p.Name = "Kumhof (IN MEMORY)";
-                p.Latitude = 59.331181;
-                p.Longitude = 18.040736;
-            });
-            
-            var trornehof = Course.Create(patrik, p =>
-            {
-                p.Holes = 6;
-                p.Name = "Törnehof (IN MEMORY)";
-            });
-            
-            context.Add(kumhof);
-            context.Add(trornehof);
-
-            for (int r = 0; r < 25; r++)
-            {
-                var roundKumhof = Round.Create(kumhof);
-                var random = new Random();
-                
-                for (int hole = 1; hole <= kumhof.Holes; hole++)
-                {
-                    roundKumhof.CreateRoundScore(jimmy, hole, random.Next(1, 6));
-                    roundKumhof.CreateRoundScore(patrik, hole, random.Next(1, 6));
-                }
-
-                context.Add(roundKumhof);
-            }
-            
-            await context.SaveChangesAsync();
-        }
+        
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
---
+++ Tradgardsgolf.Api/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tradgardsgolf.Core.Entities;
+using Tradgardsgolf.Infrastructure.Database;
+
+namespace Tradgardsgolf.Api;
+
+public static partial class TradgardsgolfContextExtensions
+{
+    
+    private static readonly Random Random = new Random();
+    public static async Task SeedData(this TradgardsgolfContext context)
+    {
+        var jimmy = Player.Create(p => p.Name = "Jimmy");
+        var patrik = Player.Create(p => p.Name = "Patrik");
+            
+        context.Add(jimmy);
+        context.Add(patrik);
+            
+        var kumhof = Course.Create(jimmy, p =>
+        {
+            p.Holes = 6;
+            p.Name = "Kumhof (IN MEMORY)";
+            p.Latitude = 59.331181;
+            p.Longitude = 18.040736;
+        });
+            
+        var trornehof = Course.Create(patrik, p =>
+        {
+            p.Holes = 6;
+            p.Name = "Törnehof (IN MEMORY)";
+        });
+            
+        context.Add(kumhof);
+        context.Add(trornehof);
+
+        for (int r = 0; r < 25; r++)
+            AddRound(context, kumhof, jimmy, patrik);
+            
+        await context.SaveChangesAsync();
+    }
+
+    private static void AddRound(this TradgardsgolfContext context, Course kumhof, params Player[] players)
+    {
+        var round = Round.Create(kumhof);
+
+        for (int hole = 1; hole <= kumhof.Holes; hole++)
+        {
+            foreach (var player in players)
+                round.CreateRoundScore(player, hole, GenerateRandomScore());
+        }
+
+        context.Add(round);
+    }
+
+    private static int GenerateRandomScore()
+    {
+        var score = Random.Next(0, 100);
+
+        return score switch
+        {
+            >= 95 => 1,
+            >= 85 => 2,
+            >= 50 => 3,
+            >= 30 => 4,
+            >= 15 => 5,
+            _ => 6
+        };
+
+    }
+}
