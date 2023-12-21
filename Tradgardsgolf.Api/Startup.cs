4fb0c77098bb9e7b734a1b46af75b85fe9e8d6e7
Jimmy <vaugh@hotmail.se>
2023-12-21 07:57:31 +0100

    Removed unused references

--- Tradgardsgolf.Api/Startup.cs
+++ Tradgardsgolf.Api/Startup.cs
@@ -1,24 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Tradgardsgolf.Api.RequestHandling;
 using Tradgardsgolf.Core.Infrastructure;
-using Tradgardsgolf.Infrastructure;
 using Tradgardsgolf.Infrastructure.Database;
 
 namespace Tradgardsgolf.Api
