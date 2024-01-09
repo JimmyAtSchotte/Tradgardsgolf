55f22564b4b41ca4de36e6d51198164e2a60368f
Jimmy <vaugh@hotmail.se>
2024-01-09 09:52:31 +0100

    Revert "Reading setting from environment variables"
    This reverts commit d1fca7f219e82c55c06488b320d6f914be802696.

--- Tradgardsgolf.Blazor.Wasm/Program.cs
+++ Tradgardsgolf.Blazor.Wasm/Program.cs
@@ -9,7 +9,6 @@ using Blazorise.Icons.Material;
 using Blazorise.Material;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.DependencyInjection;
 using Tradgardsgolf.BlazorWasm.ApiServices;
 
@@ -20,8 +19,6 @@ namespace Tradgardsgolf.BlazorWasm
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Configuration.Add(new EnvironmentVariablesConfigurationSource());
-            
             var apiUrl = builder.Configuration.GetValue<string>("API_URL");
 
             if (string.IsNullOrEmpty(apiUrl))
