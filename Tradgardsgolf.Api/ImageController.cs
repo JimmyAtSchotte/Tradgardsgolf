4a0ea1592526f3853715c235055e26eccbe8c66a
Jimmy <vaugh@hotmail.se>
2024-01-10 12:18:56 +0100

    Added image controller

---
+++ Tradgardsgolf.Api/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tradgardsgolf.Core.Infrastructure;
+
+namespace Tradgardsgolf.Api;
+
+
+[ApiController]
+[Route("images")]
+public class ImageController(IFileService fileService) : ControllerBase
+{
+    [HttpGet]
+    [Route("{filename}")]
+    public async Task<FileResult> Get([FromRoute] string filename)
+    {
+        var fileBytes = await fileService.Get(filename);
+        return new FileContentResult(fileBytes, "image/jpeg");
+    }
+}
