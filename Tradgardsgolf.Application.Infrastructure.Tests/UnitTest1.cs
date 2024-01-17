eb4377538f633aef2fde49b64313cbfb3dbf14ce
Jimmy <vaugh@hotmail.se>
2024-01-17 08:40:19 +0100

    Added new test project

---
+++ Tradgardsgolf.Application.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tradgardsgolf.Application.Infrastructure.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
