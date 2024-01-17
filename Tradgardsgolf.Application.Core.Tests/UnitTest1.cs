0d80619cb1500f2ede74dc468e49bbf45122ecf3
Jimmy <vaugh@hotmail.se>
2024-01-17 08:47:49 +0100

    Added new test project for core

---
+++ Tradgardsgolf.Application.Core.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Tradgardsgolf.Application.Core.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
