a351af9fa811bd8da03f261762f0af9761eec18f
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-10-18 10:15:11 +0200

    Add equality comparer

--- Tradgardsgolf.Application.Core/Specifications/Course/ById.cs
+++ Tradgardsgolf.Application.Core/Specifications/Course/ById.cs
@@ -3,10 +3,31 @@ using Ardalis.Specification;
 
 namespace Tradgardsgolf.Core.Specifications.Course;
 
-public class ById : Specification<Entities.Course>
+public class ById : Specification<Entities.Course> , IEquatable<ById>
 {
+    private readonly Guid _courseId;
+
     public ById(Guid courseId)
     {
+        _courseId = courseId;
         Query.Where(x => x.Id == courseId);
+    }
+
+    public bool Equals(ById other)
+    {
+        return _courseId == other._courseId;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((ById)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return _courseId.GetHashCode();
     }
 }
