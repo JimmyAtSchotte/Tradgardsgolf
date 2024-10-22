d6deb3d9c9841eefb6fdff597fd040acb54c71a7
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-10-22 10:17:18 +0200

    Test image reference

--- Tradgardsgolf.Api.RequestHandling.Tests/Course/GetCourse.cs
+++ Tradgardsgolf.Api.RequestHandling.Tests/Course/GetCourse.cs
@@ -40,5 +40,39 @@ public class GetCourse
         var result = await handler.Handle(command, CancellationToken.None);
 
         result.Id.Should().Be(course.Id);
+        result.ImageReference.Should().BeNull();
+    }
+    
+    [Test]
+    public async Task ShouldFindCourseWithImage()
+    {
+        var course = Core.Entities.Course.Create(Guid.NewGuid(), p =>
+        {
+            p.Id = Guid.NewGuid();
+            p.Image = "myimage.png";
+        });
+        
+        var arrange = Arrange.Dependencies<GetCourseHandler, GetCourseHandler>(dependencies =>
+        {
+            dependencies.UseMock<IRepository<Core.Entities.Course>>(mock =>
+            {
+                mock.Setup(x => x.FirstOrDefaultAsync(new ById(course.Id), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(course);
+            });
+
+            dependencies.UseImplementation<IResponseFactory<CourseResponse, Core.Entities.Course>, CourseResponseFactory>();
+            dependencies.UseImplementation<IResponseFactory<ImageReference, Core.Entities.Course>, ImageReferenceResponseFactory>();
+        });
+
+        var handler = arrange.Resolve<GetCourseHandler>();
+        var command = new GetCourseCommand()
+        {
+            Id = course.Id
+        };
+        
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        result.Id.Should().Be(course.Id);
+        result.ImageReference.Path.Should().Be(course.Image);
     }
 }
