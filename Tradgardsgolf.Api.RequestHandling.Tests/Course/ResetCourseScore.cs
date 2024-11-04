379b85434c69a636f5dd41e77aa4ba4c93e241b0
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-11-04 09:21:55 +0100

    Add command to reset scores from a date

---
+++ Tradgardsgolf.Api.RequestHandling.Tests/Course/ResetCourseScore.cs
@@ -0,0 +1,91 @@
+﻿using ArrangeDependencies.Autofac;
+using ArrangeDependencies.Autofac.Extensions;
+using FluentAssertions;
+using Moq;
+using Tradgardsgolf.Api.RequestHandling.Course;
+using Tradgardsgolf.Api.ResponseFactory;
+using Tradgardsgolf.Contracts.Course;
+using Tradgardsgolf.Contracts.Types;
+using Tradgardsgolf.Core.Auth;
+using Tradgardsgolf.Core.Exceptions;
+using Tradgardsgolf.Core.Infrastructure;
+using Tradgardsgolf.Core.Specifications;
+
+namespace Tradgardsgolf.Api.RequestHandling.Tests.Course;
+
+[TestFixture]
+public class ResetCourseScore
+{
+    [Test]
+    public async Task ShouldNotUpdateWhenNotTheOwner()
+    {
+        var course = Core.Entities.Course.Create(Guid.NewGuid(), p => p.Id = Guid.NewGuid());
+        var authenticatedUser = new AuthenticatedUser()
+        {
+            UserId = Guid.NewGuid(),
+        };
+        
+        var arrange = Arrange.Dependencies<ResetCourseScoreHandler, ResetCourseScoreHandler>(dependencies =>
+        {
+            dependencies.UseMock<IRepository>(mock =>
+            {
+                mock.Setup(x => x.FirstOrDefaultAsync(Specs.ById<Core.Entities.Course>(course.Id), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(course);
+            });
+            
+            dependencies.UseMock<IAuthenticationService>(mock => mock.Setup(x => x.RequireAuthenticatedUser()).Returns(authenticatedUser));
+        });
+        
+        var handler = arrange.Resolve<ResetCourseScoreHandler>();
+        var command = new ResetCourseScoreCommand()
+        {
+            CourseId = course.Id,
+            ResetDate = DateTime.Today
+        };
+        
+        await handler.Invoking(h => h.Handle(command, CancellationToken.None)).Should().ThrowAsync<ForbiddenException>();
+        course.ScoreReset.Should().BeNull();
+    }
+    
+    [Test]
+    public async Task ShouldResetScore()
+    {
+        var course = Core.Entities.Course.Create(Guid.NewGuid(), p => p.Id = Guid.NewGuid());
+        var authenticatedUser = new AuthenticatedUser()
+        {
+            UserId = course.OwnerGuid
+        };
+        
+        var repositorySpy = default(Mock<IRepository>);
+        
+        var arrange = Arrange.Dependencies<ResetCourseScoreHandler, ResetCourseScoreHandler>(dependencies =>
+        {
+            dependencies.UseMock<IRepository>(mock =>
+            {
+                mock.Setup(x => x.FirstOrDefaultAsync(Specs.ById<Core.Entities.Course>(course.Id), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(course);
+            }, out repositorySpy);
+            
+            dependencies.UseMock<IAuthenticationService>(mock => mock.Setup(x => x.RequireAuthenticatedUser()).Returns(authenticatedUser));
+            
+            
+            dependencies.UseImplementation<IResponseFactory<CourseResponse, Core.Entities.Course>, CourseResponseFactory>();
+            dependencies.UseImplementation<IResponseFactory<ImageReference, Core.Entities.Course>, ImageReferenceResponseFactory>();
+        });
+        
+        var handler = arrange.Resolve<ResetCourseScoreHandler>();
+        var command = new ResetCourseScoreCommand()
+        {
+            CourseId = course.Id,
+            ResetDate = DateTime.Today
+        };
+        
+        var response = await handler.Handle(command, CancellationToken.None);
+        course.ScoreReset.Should().Be(command.ResetDate);
+        repositorySpy.Verify(spy => spy.UpdateAsync(course, It.IsAny<CancellationToken>()), Times.Once);
+        response.ScoreReset.Should().Be(command.ResetDate);
+    }
+    
+
+    
+}
---
+++ Tradgardsgolf.Api.RequestHandling/Course/ResetCourseScoreHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Tradgardsgolf.Api.ResponseFactory;
+using Tradgardsgolf.Contracts.Course;
+using Tradgardsgolf.Core.Auth;
+using Tradgardsgolf.Core.Exceptions;
+using Tradgardsgolf.Core.Infrastructure;
+using Tradgardsgolf.Core.Specifications;
+
+namespace Tradgardsgolf.Api.RequestHandling.Course;
+
+public class ResetCourseScoreHandler : IRequestHandler<ResetCourseScoreCommand, CourseResponse>
+{
+    private readonly IRepository _repository;
+    private readonly IAuthenticationService _authenticationService;
+    private readonly IResponseFactory<CourseResponse, Core.Entities.Course> _courseResponseFactory;
+
+    public ResetCourseScoreHandler(IRepository repository, IAuthenticationService authenticationService, IResponseFactory<CourseResponse, Core.Entities.Course> courseResponseFactory)
+    {
+        _repository = repository;
+        _authenticationService = authenticationService;
+        _courseResponseFactory = courseResponseFactory;
+    }
+
+    public async Task<CourseResponse> Handle(ResetCourseScoreCommand request, CancellationToken cancellationToken)
+    {
+        var authenticatedUser = _authenticationService.RequireAuthenticatedUser();
+        var course = await _repository.FirstOrDefaultAsync(Specs.ById<Core.Entities.Course>(request.CourseId), cancellationToken);
+
+        if (course?.OwnerGuid != authenticatedUser.UserId)
+            throw new ForbiddenException();
+
+        course.ScoreReset = request.ResetDate;
+        
+        await _repository.UpdateAsync(course, cancellationToken);
+
+        return _courseResponseFactory.Create(course);
+    }
+}
+
+public class ResetCourseScoreCommand : IRequest<CourseResponse>
+{
+    public Guid CourseId { get; set; }
+    public DateTime ResetDate { get; set; }
+}
