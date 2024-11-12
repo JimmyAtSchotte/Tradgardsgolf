67e592c6b944adc1f88fedf110883bb411542362
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-11-12 12:25:29 +0100

    Use different database for tests

--- Tradgardsgolf.Application.Infrastructure.Tests/Database/TradgardsgolfContextFactory.cs
+++ Tradgardsgolf.Application.Infrastructure.Tests/Database/TradgardsgolfContextFactory.cs
@@ -8,7 +8,7 @@ public class TradgardsgolfContextFactory
     internal static TradgardsgolfContext CreateTradgardsgolfContext()
     {
         var options = new DbContextOptionsBuilder<TradgardsgolfContext>()
-            .UseCosmos("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "tradgardsgolf-db")
+            .UseCosmos("AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "tradgardsgolf-db-test")
             .Options;
 
         var context = new TradgardsgolfContext(options);
